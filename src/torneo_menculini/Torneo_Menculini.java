052bba5c8efc5fe4ee4d614db6e5a3c103698752
binf01-Iliescu <iliescurobert15@gmail.com>
2024-11-28 09:13:25 +0100

    creazione del messaggio dell'inizio torneo

--- src/torneo_menculini/Torneo_Menculini.java
+++ src/torneo_menculini/Torneo_Menculini.java
@@ -15,6 +15,7 @@ public class Torneo_Menculini {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Inizio torneo");
     }
     
 }
