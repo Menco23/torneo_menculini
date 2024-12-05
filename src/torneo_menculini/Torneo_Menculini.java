20325e5b66ef8d66113f7b0ba3546488f8b94fca
Menco23 <menculinitommaso1@gmail.com>
2024-12-05 08:30:44 +0100

    modifiche ramo master

--- src/torneo_menculini/Torneo_Menculini.java
+++ src/torneo_menculini/Torneo_Menculini.java
@@ -16,6 +16,7 @@ public class Torneo_Menculini {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Inizio torneo");
+        System.out.println("Fine torneo torneo");
     }
     
 }
